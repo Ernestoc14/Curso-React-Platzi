c7518ea5c9327424e1d2efb05947645edbda7f77
Ernestoc14 <descreed34@hotmail.com>
2023-06-02 01:48:42 -0500

    Styling the Loading Spinner

--- TODO_React_App/src/components/ToDosLoading.jsx
+++ TODO_React_App/src/components/ToDosLoading.jsx
@@ -4,6 +4,12 @@ function ToDosLoading() {
     return (
         <Box className='w-16 h-16 mb-12' >
             <Spinner
+                thickness='4px'
+                speed='0.65s'
+                color='#000000'
+                emptyColor='#707070'
+                height={'100%'}
+                width={'100%'}
             />
         </Box>
     )
